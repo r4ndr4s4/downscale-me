591a31ac3b11340b99de3969d2e7b13e87b0c307
András Rakita <Rakita.Andras@gmail.com>
2024-10-10 01:47:45 +0200

    feat: image endpoint

--- src/api/image/imageRouter.ts
+++ src/api/image/imageRouter.ts
@@ -1,24 +1,49 @@
+import axios from "axios";
 import express, { type Request, type Response, type Router } from "express";
 import asyncHandler from "express-async-handler";
-import path from "path";
 import sharp from "sharp";
+
+import getResizeOptions from "@/common/utils/getResizeOptions";
 
 export const imageRouter: Router = express.Router();
 
 imageRouter.get(
   "/",
-  asyncHandler(async (_req: Request, res: Response) => {
-    const image = await sharp(path.resolve(__dirname, "img.jpg"))
-      //.rotate()
-      .resize(200)
-      // .jpeg({ mozjpeg: true })
-      .toBuffer();
+  asyncHandler(async (_: Request, res: Response) => {
+    res.status(404).send("Missing image URL");
+  })
+);
 
-    res
-      .writeHead(200, {
-        "Content-Type": "image/jpeg",
-        "Content-Length": image.length,
-      })
-      .end(image);
+imageRouter.get(
+  "/:href(*)",
+  asyncHandler(async ({ params: { href }, query }: Request, res: Response) => {
+    const paramsStartAt = href.lastIndexOf("/");
+    const imageUrl = href.slice(0, paramsStartAt);
+
+    const { w, h } = query;
+
+    console.log({ href, imageUrl, query, w, h });
+
+    try {
+      const { data: image } = await axios({
+        url: imageUrl,
+        responseType: "stream",
+      });
+
+      const process = sharp();
+
+      if (w || h) {
+        const resizeOptions = getResizeOptions(w?.toString(), h?.toString());
+
+        process.resize(resizeOptions);
+      }
+
+      res.type("image/jpeg");
+      image.pipe(process).pipe(res);
+    } catch (error) {
+      console.log({ error });
+
+      res.status(500).send("Error processing image");
+    }
   })
 );
